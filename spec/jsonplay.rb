8a99ec70a2f803028058184c2a4a5590ab362d89
alfonso rush <alephmage@gmail.com>
2012-10-08 14:50:47 -0500

    ok object works and can assign variables as pop asked for...better i think

--- spec/jsonplay.rb
+++ spec/jsonplay.rb
@@ -1,4 +1,4 @@
-# require	'open-uri'
+require	'open-uri'
 require 'pp' #pretty printed output...wow
 require 'net/http'
 require 'json' # oh this is freaky...without this line json pulls a false error...it's all over the net this week-oct 8th
@@ -14,13 +14,13 @@ class Jsonplay
 		res = Net::HTTP.post_form(uri, 'email' => 'integration-tester+1@benchprep.com')
 
 		# write this output to a file
-		output = File.open( "fixtures/sp_wm_persona.json",'w' ){|f| 
+		output = File.open( "../fixtures/sp_wm_persona.json",'w' ){|f| 
 			f.flock(File::LOCK_EX)
 			f.write(res.body)
 		}
 
 		# Now parse this string as json
-		json = File.read('fixtures/sp_wm_persona.json')
+		json = File.read('../fixtures/sp_wm_persona.json')
 		@empls = JSON.parse(json)
 
 		return @empls #pretty printed output
