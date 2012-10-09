c1f255b3e5a3b246244ddc70284c62a98c65f027
alfonso rush <alephmage@gmail.com>
2012-10-09 13:58:18 -0500

    able to click each li in loop...but not external link yet...will selenium do what I want?

--- spec/requests/courses_page_spec.rb
+++ spec/requests/courses_page_spec.rb
@@ -6,46 +6,25 @@ describe "https://benchprep.com/courses page" do
 
 		page.should have_content("Browse BenchPrep Courses")
 	end
+	# TODO loop over each li within ul#course-list
+	# iterate each li item in ul#course-list
 
-	it 'should be able to visit ACT English Prep Course' do
-		visit('/courses')
-		# find h3 course title with course name
-		within(:xpath, "//li[@data-content-package-id='182']")do
-			find('h3.course-title').click
-			page.should have_link("Learn more")
+	
+		it 'should be able to visit each Course' do
+			visit('/courses')
+			# find h3 course title with course name
+			within('ul#course-list.course-list') do
+				all('li.course.course-list-item').each { |item|
+					# find('h3.course-title').click
+					# page.should have_link("Learn more")
 
-			within(:xpath, "//li[@data-content-package-id='182']")do
-				click_on('Learn more')
+					item.click
+					# within(:xpath, ".//li")do
+						
+					# end
+				}
+				# page.should_not have_content('500 internal')
 			end
 		end
-		find('h1.padded-heading').should have_content('ACT English Prep Course')
-	end
-	it 'should be able to visit ACT Math Exam Prep Course' do
-		visit('/courses')
-		# find h3 course title with course name
-		within(:xpath, "//li[@data-content-package-id='191']")do
-			find('h3.course-title').click
-			page.should have_link("Learn more")
-
-			within(:xpath, "//li[@data-content-package-id='191']")do
-				click_on('Learn more')
-			end
-		end
-		find('h1.padded-heading').should have_content('ACT Math Exam Prep Course')
-	end
-	it 'should be able to visit Biochemistry Prep Course - Schaums' do
-		visit('/courses')
-		# find h3 course title with course name
-		within(:xpath, "//li[@data-content-package-id='134']")do
-			find('h3.course-title').click
-			page.should have_link("Learn more")
-
-			within(:xpath, "//li[@data-content-package-id='134']")do
-				click_on('Learn more')
-			end
-		end
-		find('h1.padded-heading').should have_content('Biochemistry Prep Course - Schaums Outlines')
-	end
-
+	
 end
-
