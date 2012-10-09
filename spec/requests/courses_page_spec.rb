728c2dd79af5ed85d4a5e6de5d996b494e7a494f
alfonso rush <alephmage@gmail.com>
2012-10-09 16:03:53 -0500

    cant convert nil to string error

--- spec/requests/courses_page_spec.rb
+++ spec/requests/courses_page_spec.rb
@@ -1,51 +1,33 @@
 require_relative '../../spec_helper'
 
 describe "https://benchprep.com/courses page" do
-	xit 'should go to the all courses page' do
-		visit('/courses')
 
-		page.should have_content("Browse BenchPrep Courses")
-	end
+	tests = [] #init array
+	it 'should be able to visit each Course' do
 
-	it 'should be able to visit ACT English Prep Course' do
 		visit('/courses')
 		# find h3 course title with course name
-		within(:xpath, "//li[@data-content-package-id='182']")do
-			find('h3.course-title').click
-			page.should have_link("Learn more")
+		within('ul#course-list.course-list')do
+			all('li').each { |item|
+			
+			# Pass one
+			title = 'h3#{value}'
+			id = item["data-content-package-id"]
+			# tests = << {title: title, id: id}
 
-			within(:xpath, "//li[@data-content-package-id='182']")do
-				click_on('Learn more')
+			within(:xpath, "//li[@data-content-package-id='"+id+"']")do
+				find('h3.course-title').click
+
+				page.should have_link("Learn more")
+
+				within(:xpath, "//li[@data-content-package-id='"+id+"']")do
+					click_on('Learn more')
+				end
 			end
-		end
-		find('h1.padded-heading').should have_content('ACT English Prep Course')
-	end
-	it 'should be able to visit ACT Math Exam Prep Course' do
-		visit('/courses')
-		# find h3 course title with course name
-		within(:xpath, "//li[@data-content-package-id='191']")do
-			find('h3.course-title').click
-			page.should have_link("Learn more")
+			}
 
-			within(:xpath, "//li[@data-content-package-id='191']")do
-				click_on('Learn more')
-			end
+			find('h1.padded-heading').should have_content(title)
 		end
-		find('h1.padded-heading').should have_content('ACT Math Exam Prep Course')
 	end
-	it 'should be able to visit Biochemistry Prep Course - Schaums' do
-		visit('/courses')
-		# find h3 course title with course name
-		within(:xpath, "//li[@data-content-package-id='134']")do
-			find('h3.course-title').click
-			page.should have_link("Learn more")
-
-			within(:xpath, "//li[@data-content-package-id='134']")do
-				click_on('Learn more')
-			end
-		end
-		find('h1.padded-heading').should have_content('Biochemistry Prep Course - Schaums Outlines')
-	end
-
 end
 
